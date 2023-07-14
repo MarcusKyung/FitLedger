dc23cb7fb258081b4c385c3b8d2032e3fc738c79
MarcusKyung <Kyungmj@gmail.com>
2023-07-14 09:04:40 -0700

    add Firebase configuration/setup and dependencies

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom"; 
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { auth } from "../firebase";
+
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <React.Fragment>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand>Epicodus Help Queue</Navbar.Brand>
+            <Nav className="ms-auto">
+            <Navbar.Text className="ml-3">
+              {props.currentUser ? (`Currently Signed in: ${props.currentUser.email}`) : ("Not Signed in")}
+            </Navbar.Text>              
+            <Button style={{ marginLeft: "1rem", }} variant="outline-primary"><Link to="/sign-in">{props.currentUser ? "Sign Out" : "Sign In"}</Link></Button>
+            <Button style={{ marginLeft: "1rem", }} variant="outline-primary"><Link to="/">Home</Link></Button>
+            </Nav>
+        </Container>
+      </Navbar>
+    </React.Fragment>  )
 }
