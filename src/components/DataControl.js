24a12dd80c45f05c46243530b546b99008354570
MarcusKyung <Kyungmj@gmail.com>
2023-07-16 10:30:40 -0700

    WIP: reusable form/DataControl update

--- src/components/DataControl.js
+++ src/components/DataControl.js
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import NewDataForm from './NewDataForm'
+import DataList from './DataList'
+import DataDetail from './DataDetail'
+import EditDataForm from './EditDataForm'
+import { db, auth } from "./../firebase.js";
+import { collection, addDoc, doc, updateDoc, onSnapshot, deleteDoc, query, where, orderBy } from "firebase/firestore"; //Import Firestore helper functions
+import { formatDistanceToNow } from 'date-fns';
 
-export default function DataControl() {
+function DataControl() {
+  const [formVisibleOnPage, setFormVisibleOnPage] = useState(false);
+  const [mainDataList, setMainDataList] = useState([]);
+  const [selectedData, setSelectedData] = useState(null);
+  const [editing, setEditing] = useState(false);
+  const [error, setError] = useState(null);
+
+
+  if (auth.currentUser == null) {
+    return (
+      <React.Fragment>
+        <Row>
+          <Col />
+          <Col>
+            <h1 style={{ color: "red" , marginTop: "1em"}}>You must be signed in to access the queue!</h1>
+          </Col>
+          <Col />
+        </Row>
+      </React.Fragment>
+    );
+  } else if (auth.currentUser != null) {
+    let currentlyVisibleState = null;
+    let buttonText = null;
+
   return (
-    <div>DataControl</div>
-  )
+    <React.Fragment>
+      {currentlyVisibleState}
+      <ButtonGroup>
+        {error ? null : <Button variant="primary" onClick={handleClick}>{buttonText}</Button>}
+      </ButtonGroup>
+    </React.Fragment>
+    );
+  }
 }
+
+export default TicketControl;
+
--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 function ReusableForm(props) {
   return (
@@ -13,7 +14,6 @@ function ReusableForm(props) {
           <Form.Control type="number" placeholder="Meal 1 Carbs (g)" />
           <Form.Control type="number" placeholder="Meal 1 Fats (g)" />
         </Form.Group>
-
         <Form.Group className="mb-3" controlId="formMeal2Data">
           <Form.Control type="text" placeholder="Meal 1 Name" />
           <Form.Control type="number" placeholder="Meal 2 Calories (cal)" />
@@ -21,7 +21,6 @@ function ReusableForm(props) {
           <Form.Control type="number" placeholder="Meal 2 Carbs (g)" />
           <Form.Control type="number" placeholder="Meal 2 Fats (g)" />
         </Form.Group>
-
         <Form.Group className="mb-3" controlId="formMeal3Data">
           <Form.Control type="text" placeholder="Meal 3 Name" />
           <Form.Control type="number" placeholder="Meal 3 Calories (cal)" />
@@ -29,80 +28,82 @@ function ReusableForm(props) {
           <Form.Control type="number" placeholder="Meal 3 Carbs (g)" />
           <Form.Control type="number" placeholder="Meal 3 Fats (g)" />
         </Form.Group>
-
         <Form.Group className="mb-3" controlId="formWaterIntakeData">
           <Form.Control type="number" placeholder="Daily Water Intake (oz)" />
         </Form.Group>
-
         <Form.Group className="mb-3" controlId="formSupplement1Data">
           <Form.Control type="text" placeholder="Supplement 1 Name" />
           <Form.Control type="number" placeholder="Supplement 1 Amount (mg)" />
         </Form.Group>
-
         <Form.Group className="mb-3" controlId="formSupplement2Data">
           <Form.Control type="text" placeholder="Supplement 2 Name" />
           <Form.Control type="number" placeholder="Supplement 2 Amount (mg)" />
         </Form.Group>
-
         <Form.Group className="mb-3" controlId="formSupplement2Data">
           <Form.Control type="text" placeholder="Supplement 2 Name" />
           <Form.Control type="number" placeholder="Supplement 2 Amount (mg)" />
         </Form.Group>
-
         <hr />
-
-        <input type='text' name='exercise1Name' placeholder='Exercise 1 Name' />
-        <input type='number' name='exercise1Sets' placeholder='Exercise 1 Sets' />
-        <input type='number' name='exercise1Reps' placeholder='Exercise 1 Reps' />
-        <input type='number' name='exercise1Weight' placeholder='Exercise 1 Weight' />
-        <input type='text' name='exercise1Notes' placeholder='Exercise 1 Notes' />
-
-        <input type='text' name='exercise2Name' placeholder='Exercise 2 Name' />
-        <input type='number' name='exercise2Sets' placeholder='Exercise 2 Sets' />
-        <input type='number' name='exercise2Reps' placeholder='Exercise 2 Reps' />
-        <input type='number' name='exercise2Weight' placeholder='Exercise 2 Weight' />
-        <input type='text' name='exercise2Notes' placeholder='Exercise 2 Notes' />
-
-        <input type='text' name='exercise3Name' placeholder='Exercise 3 Name' />
-        <input type='number' name='exercise3Sets' placeholder='Exercise 3 Sets' />
-        <input type='number' name='exercise3Reps' placeholder='Exercise 3 Reps' />
-        <input type='number' name='exercise3Weight' placeholder='Exercise 3 Weight' />
-        <input type='text' name='exercise3Notes' placeholder='Exercise 3 Notes' />
-
-        <input type='text' name='exercise4Name' placeholder='Exercise 4 Name' />
-        <input type='number' name='exercise4Sets' placeholder='Exercise 4 Sets' />
-        <input type='number' name='exercise4Reps' placeholder='Exercise 4 Reps' />
-        <input type='number' name='exercise4Weight' placeholder='Exercise 4 Weight' />
-        <input type='text' name='exercise4Notes' placeholder='Exercise 4 Notes' />
-
-        <input type='text' name='exercise5Name' placeholder='Exercise 5 Name' />
-        <input type='number' name='exercise5Sets' placeholder='Exercise 5 Sets' />
-        <input type='number' name='exercise5Reps' placeholder='Exercise 5 Reps' />
-        <input type='number' name='exercise5Weight' placeholder='Exercise 5 Weight' />
-        <input type='text' name='exercise5Notes' placeholder='Exercise 5 Notes' />
-
-        <select name="cardioName">
-          <option value="" disabled selected>Select Cardio Type</option>
-          <option value="Run/Walk">Run/Walk</option>
-          <option value="Elliptical">Elliptical</option>
-          <option value="Cycling">Cycling</option>
-          <option value="Stairmaster">Stairmaster</option>
-          <option value="Swim">Swim</option>
-        </select>        
-        <input type='number' name='cardioDuration' placeholder='Cardio Duration' />
-        <input type='number' name='cardioDistance' placeholder='Cardio Distance' />
-        <input type='text' name='cardioNote' placeholder='Cardio Note' />
-
+        <Form.Group className="mb-3" controlId="formExercise1Data">
+          <Form.Control type="text" placeholder="Exercise 1 Name" />
+          <Form.Control type="number" placeholder="Exercise 1 Sets" />
+          <Form.Control type="number" placeholder="Exercise 1 Reps" />
+          <Form.Control type="number" placeholder="Exercise 1 Weight" />
+          <Form.Control type="text" placeholder="Exercise 1 Notes" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formExercise2Data">
+          <Form.Control type="text" placeholder="Exercise 2 Name" />
+          <Form.Control type="number" placeholder="Exercise 2 Sets" />
+          <Form.Control type="number" placeholder="Exercise 2 Reps" />
+          <Form.Control type="number" placeholder="Exercise 2 Weight" />
+          <Form.Control type="text" placeholder="Exercise 2 Notes" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formExercise3Data">
+          <Form.Control type="text" placeholder="Exercise 3 Name" />
+          <Form.Control type="number" placeholder="Exercise 3 Sets" />
+          <Form.Control type="number" placeholder="Exercise 3 Reps" />
+          <Form.Control type="number" placeholder="Exercise 3 Weight" />
+          <Form.Control type="text" placeholder="Exercise 3 Notes" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formExercise4Data">
+          <Form.Control type="text" placeholder="Exercise 4 Name" />
+          <Form.Control type="number" placeholder="Exercise 4 Sets" />
+          <Form.Control type="number" placeholder="Exercise 4 Reps" />
+          <Form.Control type="number" placeholder="Exercise 4 Weight" />
+          <Form.Control type="text" placeholder="Exercise 4 Notes" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formExercise5Data">
+          <Form.Control type="text" placeholder="Exercise 5 Name" />
+          <Form.Control type="number" placeholder="Exercise 5 Sets" />
+          <Form.Control type="number" placeholder="Exercise 5 Reps" />
+          <Form.Control type="number" placeholder="Exercise 5 Weight" />
+          <Form.Control type="text" placeholder="Exercise 5 Notes" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formCardioData">
+          <Form.Label>Select Cardio Type</Form.Label>
+          <Form.Select>
+            <option value="" disabled selected>Select Cardio Type</option>
+            <option value="Run/Walk">Run/Walk</option>
+            <option value="Elliptical">Elliptical</option>
+            <option value="Cycling">Cycling</option>
+            <option value="Stairmaster">Stairmaster</option>
+            <option value="Swim">Swim</option>
+          </Form.Select>
+          <Form.Control type="text" placeholder="Cardio Duration" />
+          <Form.Control type="number" placeholder="Cardio Distance" />
+          <Form.Control type="text" placeholder="Cardio Note" />
+        </Form.Group>
         <hr />
-
-        <input type='time' name='sleepTime' placeholder='Sleep Time' />
-        <input type='time' name='wakeTime' placeholder='Wake Time' />
-        <input type='text' name='sleepDescription' placeholder='Sleep Quality Description' />
-        <input type='number' name='sleepScore' placeholder='Sleep Score 1-10' min="1" max="10" />
-
-        <input type='number' name='dailyWeight' placeholder='Daily Weight (lbs)' />
-
-        <button type='submit'>{props.buttonText}</button>
+        <Form.Group className="mb-3" controlId="formSleepData">
+          <Form.Control type="time" placeholder="Sleep Time" />
+          <Form.Control type="time" placeholder="Wake Time" />
+          <Form.Control type="text" placeholder="Sleep Quality Description" />
+          <Form.Control type="number" placeholder="Sleep Score 1-10" min="1" max="10"/>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formSleepData">
+          <Form.Control type="number" placeholder="Daily Weight (lbs)" />
+        </Form.Group>
+        <Button type='submit'>{props.buttonText}</Button>
       </Form>
     </React.Fragment>
   );
