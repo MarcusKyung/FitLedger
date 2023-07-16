acb70bb4a0f577e97be1c983050e12098ab7b7af
MarcusKyung <Kyungmj@gmail.com>
2023-07-16 11:06:17 -0700

    add Quotes component

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import DataControl from './DataControl';
-import VictoryTest from './VictoryTest'; //Will need to live in DataControl in order to use props
+import DailyQuote from './DailyQuote';
+// import VictoryTest from './VictoryTest'; //Will need to live in DataControl in order to use props
 
 function App() {
   return (
     <React.Fragment>
-    {/* <Header />
-    <DataControl /> */}
-    <VictoryTest />
+    <Header />
+    <DailyQuote />
+    <DataControl />
+    {/* <VictoryTest /> */}
     </React.Fragment>
   );
 }
