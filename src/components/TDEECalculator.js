5e183f714381eb143d7aec7d2fa28e2eac9cc0f5
MarcusKyung <Kyungmj@gmail.com>
2023-07-23 23:09:42 -0700

    add: update TDEE calculator to take lbs input

--- src/components/TDEECalculator.js
+++ src/components/TDEECalculator.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Row, Col } from 'react-bootstrap';
 
 function calculateTDEE(bodyWeight, bodyHeight, userAge, activityFactor) {
-  const bmr = 10 * bodyWeight + 6.25 * bodyHeight - 5 * userAge + 5;
+  const bmr = 10 * (bodyWeight * 0.45359237) + 6.25 * (bodyHeight * 2.54) - 5 * userAge + 5;
   const tdee = bmr * activityFactor;
   return tdee;
 }
@@ -21,25 +21,32 @@ export default function TDEECalculator() {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group controlId="tDEEInput">
-        <Form.Control type="number" name="bodyWeight" placeholder="Body Weight in kg" />
-        <Form.Control type="number" name="bodyHeight" placeholder="Height in cm" />
-        <Form.Control type="number" name="userAge" placeholder="Age" />
-        <Form.Select defaultValue="" name="activityFactor">
-          <option disabled>Select Daily Status</option>
-          <option value="1.2">Sedentary</option>
-          <option value="1.375">Lightly Active</option>
-          <option value="1.55">Moderately Active</option>
-          <option value="1.725">Very Active</option>
-          <option value="1.9">Super Active</option>
-        </Form.Select>
-        <Button type="submit">Calculate TDEE</Button>
-      </Form.Group>
-
-      {tdee !== null && (
-        <p>Your Total Daily Energy Expenditure (TDEE) is: {tdee.toFixed(2)}</p>
-      )}
-    </Form>
+    <React.Fragment>
+      <Row>
+        <Col />
+        <Col>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="tDEEInput">
+            <Form.Control type="number" name="bodyWeight" placeholder="Body Weight in lbs" required/>
+            <Form.Control type="number" name="bodyHeight" placeholder="Height in inches" required/>
+            <Form.Control type="number" name="userAge" placeholder="Age" required/>
+            <Form.Select defaultValue="" name="activityFactor" required>
+              <option disabled>Select Daily Status</option>
+              <option value="1.2">Sedentary</option>
+              <option value="1.375">Lightly Active</option>
+              <option value="1.55">Moderately Active</option>
+              <option value="1.725">Very Active</option>
+              <option value="1.9">Super Active</option>
+            </Form.Select>
+            <Button type="submit">Calculate TDEE</Button>
+          </Form.Group>
+          {tdee !== null && (
+            <p>Your Total Daily Energy Expenditure (TDEE) is: {tdee.toFixed(2)}</p>
+          )}
+        </Form>
+        </Col>
+        <Col />
+      </Row>
+    </React.Fragment>
   );
 }
