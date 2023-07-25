bbd905b9561ab6889495b4e0d81fab2f0dabdcd4
MarcusKyung <Kyungmj@gmail.com>
2023-07-25 16:38:08 -0700

    Add: navbar reformat

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom"; 
-import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
 
 function Header(props) {
@@ -13,9 +13,21 @@ function Header(props) {
             <Navbar.Text className="ml-3">
               {props.currentUser ? (`Currently Signed in as: ${props.currentUser.email}`) : ("Not Signed in")}
             </Navbar.Text>              
-            <Button style={{ marginLeft: "1rem", }} variant="outline-primary"><Link to="/sign-in">{props.currentUser ? "Sign Out" : "Sign In"}</Link></Button>
-            <Button style={{ marginLeft: "1rem", }} variant="outline-primary"><Link to="/">Home</Link></Button>
-            <Button style={{ marginLeft: "1rem", }} variant="outline-primary"><Link to="tdee">TDEE</Link></Button>
+            <Link to={props.currentUser ? "/sign-out" : "/sign-in"}>
+              <Button style={{ marginLeft: "1rem" }} variant="outline-primary">
+                {props.currentUser ? "Sign Out" : "Sign In"}
+              </Button>
+            </Link>
+            <Link to="/">
+              <Button style={{ marginLeft: "1rem" }} variant="outline-primary">Home</Button>
+            </Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item eventKey="1" as={Link} to="/tdee">TDEE Calculator</NavDropdown.Item>
+              <NavDropdown.Item eventKey="2" as={Link} to="/1rm">1RM Calculator</NavDropdown.Item>
+              <NavDropdown.Item eventKey="3" href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item eventKey="4" href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
             </Nav>
         </Container>
       </Navbar>
