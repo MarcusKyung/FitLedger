5b5d774955720ee66cd336566afa87b3f8c05332
MarcusKyung <Kyungmj@gmail.com>
2023-07-25 08:46:53 -0700

    add: data card formatting

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,22 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Card from "react-bootstrap/Card";
-import { Col, Row } from "react-bootstrap";
+import { Container, Col, Row } from "react-bootstrap";
+
+// function calculateTotalCals(meal1Calories, meal2Calories, meal3Calories) {
+//   const dailyCals = meal1Calories+meal2Calories+meal3Calories;
+//   return dailyCals;
+// }
+
+
 
 function Data(props){
 
   return (
     <React.Fragment>
-      <Row>
-        <Col />
-        <Col xs={6}>
-          <Card onClick = {() => props.whenDataClicked(props.id)}> 
-            <h1 style={{textAlign: "center"}}>{props.entryDate}</h1>
-          </Card>
-          <hr />
-        </Col>
-        <Col />
-      </Row>
+      <Container>
+        <Row>
+          <Col />
+          <Col xs={6}>
+            <Card style={{width: "200px", height: "200px"}} onClick = {() => props.whenDataClicked(props.id)}> 
+              <h3 style={{textAlign: "center"}}>{props.entryDate}</h3>
+              <p style={{textAlign: "center", outline: "2px solid red"}}><em>{props.dailyStatus}</em></p>
+              <p style={{textAlign: "center", outline: "2px solid red"}}><em>{props.meal1Calories}</em></p>
+            </Card>
+          </Col>
+          <Col />
+        </Row>
+      </Container>
     </React.Fragment>
   );
 }
