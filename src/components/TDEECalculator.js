6f36b6c3f944ce485b333c7a74559657d5535e65
MarcusKyung <Kyungmj@gmail.com>
2023-07-25 09:10:45 -0700

    add styling to TDEE calculator

--- src/components/TDEECalculator.js
+++ src/components/TDEECalculator.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Button, Row, Col } from 'react-bootstrap';
+import { Container, Form, Button, Row, Col, Card } from 'react-bootstrap';
 
 function calculateTDEE(bodyWeight, bodyHeight, userAge, activityFactor) {
   const bmr = 10 * (bodyWeight * 0.45359237) + 6.25 * (bodyHeight * 2.54) - 5 * userAge + 5;
@@ -22,31 +22,37 @@ export default function TDEECalculator() {
 
   return (
     <React.Fragment>
-      <Row>
-        <Col />
-        <Col>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group controlId="tDEEInput">
-            <Form.Control type="number" name="bodyWeight" placeholder="Body Weight in lbs" required/>
-            <Form.Control type="number" name="bodyHeight" placeholder="Height in inches" required/>
-            <Form.Control type="number" name="userAge" placeholder="Age" required/>
-            <Form.Select defaultValue="" name="activityFactor" required>
-              <option disabled>Select Daily Status</option>
-              <option value="1.2">Sedentary</option>
-              <option value="1.375">Lightly Active</option>
-              <option value="1.55">Moderately Active</option>
-              <option value="1.725">Very Active</option>
-              <option value="1.9">Super Active</option>
-            </Form.Select>
-            <Button type="submit">Calculate TDEE</Button>
-          </Form.Group>
-          {tdee !== null && (
-            <p>Your Total Daily Energy Expenditure (TDEE) is: {tdee.toFixed(2)}</p>
-          )}
-        </Form>
-        </Col>
-        <Col />
-      </Row>
+      <Container style={{marginTop: "10px"}}>
+        <Row>
+          <Col />
+          <Col xs={6}>
+            <Card>
+              <Card.Body>
+                <Card.Title>TDEE (Total Daily Energy Expenditure)</Card.Title>
+                <Card.Subtitle>TDEE = BMR (Basal Metabolic Rate) * (Activity Factor)</Card.Subtitle>
+                <Form onSubmit={handleSubmit} style={{marginTop: "10px"}}>
+                  <Form.Group controlId="tDEEInput">
+                    <Form.Control type="number" name="bodyWeight" placeholder="Body Weight in lbs" min="0" required/>
+                    <Form.Control type="number" name="bodyHeight" placeholder="Height in inches" min="0" required/>
+                    <Form.Control type="number" name="userAge" placeholder="Age" min="0" required/>
+                    <Form.Select defaultValue="" name="activityFactor" required>
+                      <option disabled>Select Daily Status</option>
+                      <option value="1.2">Sedentary</option>
+                      <option value="1.375">Lightly Active</option>
+                      <option value="1.55">Moderately Active</option>
+                      <option value="1.725">Very Active</option>
+                      <option value="1.9">Super Active</option>
+                    </Form.Select>
+                    <Button style={{marginTop: "10px"}} type="submit">Calculate TDEE</Button>
+                  </Form.Group>
+                  {tdee !== null && (<p>Your Total Daily Energy Expenditure (TDEE) is: {tdee.toFixed(2)}</p>)}
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col />
+        </Row>
+      </Container>
     </React.Fragment>
   );
 }
