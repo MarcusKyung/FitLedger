d5cca39b76a3d8c12655b12e30fc49f5bc8d385e
MarcusKyung <Kyungmj@gmail.com>
2023-08-02 22:27:00 -0700

    edit: update accordions to modals

--- src/components/VictoryControl.js
+++ src/components/VictoryControl.js
@@ -1,48 +1,130 @@
-import React from 'react';
-import Accordion from "react-bootstrap/Accordion";
-import WaterIntakeChart from './WaterIntakeChart';
-import TotalCaloriesChart from './TotalCaloriesChart';
-import MacrobreakdownChart from './MacrobreakdownChart';
+import React, { useState } from 'react';
+import { Col, Button, Modal, Container, Row } from 'react-bootstrap';
 import DailyWeightChart from './DailyWeightChart';
+import WaterIntakeChart from './WaterIntakeChart';
+import MacrobreakdownChart from './MacrobreakdownChart';
+import TotalCaloriesChart from './TotalCaloriesChart';
 import CardioBubbleChart from './CardioBubbleChart';
-
-
+import SleepRatingChart from './SleepRatingChart';
 
 export default function Victory() {
+  const [showWeightChart, setShowWeightChart] = useState(false);
+  const [showWaterIntakeChart, setShowWaterIntakeChart] = useState(false);
+  const [showMacrobreakdownChart, setShowMacrobreakdownChart] = useState(false);
+  const [showTotalCaloriesChart, setShowTotalCaloriesChart] = useState(false);
+  const [showCardioBubbleChart, setShowCardioBubbleChart] = useState(false);
+  const [showSleepRatingChart, setshowSleepRatingChart] = useState(false);
+
+  const handleClose = () => {
+    setShowWeightChart(false);
+    setShowWaterIntakeChart(false);
+    setShowMacrobreakdownChart(false);
+    setShowTotalCaloriesChart(false);
+    setShowCardioBubbleChart(false);
+    setshowSleepRatingChart(false);
+  };
+
+  const handleWeightChartShow = () => setShowWeightChart(true);
+  const handleWaterIntakeChartShow = () => setShowWaterIntakeChart(true);
+  const handleMacrobreakdownChartShow = () => setShowMacrobreakdownChart(true);
+  const handleTotalCaloriesChartShow = () => setShowTotalCaloriesChart(true);
+  const handleCardioBubbleChartShow = () => setShowCardioBubbleChart(true);
+  const handleSleepRatingChartShow = () => setshowSleepRatingChart(true);
+
   return (
     <React.Fragment>
-      <Accordion>
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>7-14 Day Total Water Intake Chart</Accordion.Header>
-          <Accordion.Body>
-            <WaterIntakeChart />
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>7-14 Day Total Caloric Intake Chart</Accordion.Header>
-          <Accordion.Body>
-            <TotalCaloriesChart />
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="2">
-          <Accordion.Header>7-14 Day Macronutrient Intake Chart</Accordion.Header>
-          <Accordion.Body>
-            <MacrobreakdownChart />
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="3">
-          <Accordion.Header>7-14 Day Daily Weight Chart</Accordion.Header>
-          <Accordion.Body>
+      <Container>
+        <Row>
+          <Col>
+            <Row>
+              <Button variant="primary" onClick={handleWeightChartShow}>Daily Weight Chart</Button>
+              <Button variant="primary" onClick={handleWaterIntakeChartShow}>Water Intake Chart</Button>
+              <Button variant="primary" onClick={handleMacrobreakdownChartShow}>Daily Macros Chart</Button>
+            </Row>
+          </Col>
+          <Col>
+            <Row>
+              <Button variant="primary" onClick={handleTotalCaloriesChartShow}>Total Calories Chart</Button>
+              <Button variant="primary" onClick={handleCardioBubbleChartShow}>Daily Cardio Chart</Button>
+              <Button variant="primary" onClick={handleSleepRatingChartShow}>Sleep Rating Chart</Button>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+
+
+      <Modal show={showWeightChart} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>7/14 Day Daily Weight Chart</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
           <DailyWeightChart />
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="4">
-          <Accordion.Header>7-14 Day Cardio Duration/Distance Chart</Accordion.Header>
-          <Accordion.Body>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={showWaterIntakeChart} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>7/14 Day Daily Water Intake</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <WaterIntakeChart />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={showMacrobreakdownChart} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>7/14 Day Macronutrient Breakdown Chart</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <MacrobreakdownChart />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={showTotalCaloriesChart} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>7/14 Day Total Caloric Intake Chart</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <TotalCaloriesChart />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={showCardioBubbleChart} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>7/14 Day Cardio Distance/Duration Chart</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
           <CardioBubbleChart />
-          </Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={showSleepRatingChart} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>7/14 Day Sleep Rating Chart</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <SleepRatingChart />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+
     </React.Fragment>
   );
 }
