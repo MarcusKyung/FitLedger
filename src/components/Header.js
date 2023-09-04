245004329df47d0fe867a3bb4884e20388743e28
MarcusKyung <Kyungmj@gmail.com>
2023-09-03 20:05:03 -0700

    update: nav dropdown to include about and home

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,14 +11,13 @@ function Header(props) {
           <Navbar.Brand><img alt="Fit-Ledger dumbbell logo" src={Logo50} width="30" height="30" className="d-inline-block align-top"/>{' '}Fit-Ledger</Navbar.Brand>
           <Nav className="ms-auto">
             <NavDropdown title="Menu" id="basic-nav-dropdown">
-              <NavDropdown.Item eventKey="1" as={Link} to="/">Home</NavDropdown.Item>
+              <NavDropdown.Item eventKey="1" as={Link} to="/about">About</NavDropdown.Item>
+              <NavDropdown.Item eventKey="2" as={Link} to="/">Home</NavDropdown.Item>
               {props.currentUser ? (<NavDropdown.Item eventKey="2" as={Link} to="/sign-in">Sign Out</NavDropdown.Item>) : (<NavDropdown.Item eventKey="2" as={Link} to="/sign-in">Sign In</NavDropdown.Item>)}
               <NavDropdown.Divider />
               <NavDropdown.Item eventKey="3" as={Link} to="/tdee">TDEE Calculator</NavDropdown.Item>
               <NavDropdown.Item eventKey="4" as={Link} to="/1rm">1RM Calculator</NavDropdown.Item>
               <NavDropdown.Item eventKey="5" as={Link} to="/workout-builder">Workout Builder</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item eventKey="6" as={Link} to="/about">About</NavDropdown.Item>
               {props.currentUser && (
                 <div>
                   <NavDropdown.Divider />
