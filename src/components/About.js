415deb85f76204a1162186e30e36875b20d5f06c
MarcusKyung <Kyungmj@gmail.com>
2023-09-15 16:24:04 -0700

    edit: update linkedin URL

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ export default function About() {
             This project was created by Marcus Kyung as a capstone for the Epicodus full stack software engineering bootcamp. Fit-Ledger is a program designed to be a single source of truth for an athlete's health and fitness data. To use this application simply fill out the daily data entry form on the homepage. Data entries are stored in Google's Firestore Database and are accessible only to the user to which it belongs.
             <div style={{ marginTop: "10px" }} className="d-grid gap-2">
               <Button style={{ textDecoration: 'none' }}>
-                <a href="https://github.com/MarcusKyung/FitLedger" target="_blank" rel="noreferrer" style={{ textDecoration: 'none', color: 'white', display: 'block', width: '100%', height: '100%' }}>View Source Code <FaGithub /></a>
+                <a href="https://www.linkedin.com/in/marcuskyung/" target="_blank" rel="noreferrer" style={{ textDecoration: 'none', color: 'white', display: 'block', width: '100%', height: '100%' }}>View Source Code <FaGithub /></a>
               </Button>
             </div>
             </Accordion.Body>
