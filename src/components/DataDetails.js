b05b24fa24fb52853774d523c83bd8553968d7cd
MarcusKyung <Kyungmj@gmail.com>
2023-09-15 15:50:39 -0700

    edit: reformat to remove <p> and fix DOMvalidation

--- src/components/DataDetails.js
+++ src/components/DataDetails.js
@@ -5,9 +5,6 @@ import { VictoryPie } from 'victory';
 
 export default function DataDetails(props) {
   const { data, onClickingDelete, onClickingEdit } = props;
-
-  // Fix this: thecodeforge.io/post/quickly-fix-validatedomnesting-div-cannot-appear-as-a-descendant-of-p-tag-error
-  //Issue persists
   
   return (
     <React.Fragment>
@@ -24,60 +21,53 @@ export default function DataDetails(props) {
                 <Accordion.Item eventKey="0">
                   <Accordion.Header>Meal 1: {data.meal1Name}</Accordion.Header>
                   <Accordion.Body>
-                    <p>{data.meal1Calories} Calories</p>
-                    <p>{data.meal1Protein} g Protein</p>
-                    <p>{data.meal1Carbs} g Carbs</p>
-                    <p>{data.meal1Fats} g Fats</p>
+                    {data.meal1Calories} Calories <br />
+                    {data.meal1Protein} g Protein <br />
+                    {data.meal1Carbs} g Carbs <br />
+                    {data.meal1Fats} g Fats <br />
                   </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="1">
                   <Accordion.Header>Meal 2: {data.meal2Name}</Accordion.Header>
                   <Accordion.Body>
-                    <p>{data.meal2Calories} Calories</p>
-                    <p>{data.meal2Protein} g Protein</p>
-                    <p>{data.meal2Carbs} g Carbs</p>
-                    <p>{data.meal2Fats} g Fats</p>
+                    {data.meal2Calories} Calories <br />
+                    {data.meal2Protein} g Protein <br />
+                    {data.meal2Carbs} g Carbs <br />
+                    {data.meal2Fats} g Fats <br />
                   </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="2">
                   <Accordion.Header>Meal 3: {data.meal3Name}</Accordion.Header>
                   <Accordion.Body>
-                    <p>{data.meal3Calories} Calories</p>
-                    <p>{data.meal3Protein} g Protein</p>
-                    <p>{data.meal3Carbs} g Carbs</p>
-                    <p>{data.meal3Fats} g Fats</p>
+                    {data.meal3Calories} Calories <br />
+                    {data.meal3Protein} g Protein <br />
+                    {data.meal3Carbs} g Carbs <br />
+                    {data.meal3Fats} g Fats <br />
                   </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="3">
                   <Accordion.Header>Fluids and Supplement Data</Accordion.Header>
                   <Accordion.Body>
-                    <p>{data.waterIntake} oz</p>
-                    <p>
-                      {data.supplement1Name} - {data.supplement1Amount} mg
-                    </p>
-                    <p>
-                      {data.supplement2Name} - {data.supplement2Amount} mg
-                    </p>
-                    <p>
-                      {data.supplement3Name} - {data.supplement3Amount} mg
-                    </p>
+                    {data.waterIntake} oz <br />
+                    {data.supplement1Name} - {data.supplement1Amount} mg <br />
+                    {data.supplement2Name} - {data.supplement2Amount} mg <br />
+                    {data.supplement3Name} - {data.supplement3Amount} mg <br />
                   </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="4">
                   <Accordion.Header>Caloric Breakdown by Meal:</Accordion.Header>
                   <Accordion.Body>
-
-                  <VictoryPie
-                    height={300}
-                    labelRadius={({ innerRadius }) => innerRadius + 40} 
-                    labels={({ datum }) => `${datum.x}\n${datum.y} g`} 
-                    data={[
-                      { x: "Meal 1", y: data.meal1Calories },
-                      { x: "Meal 2", y: data.meal2Calories },
-                      { x: "Meal 3", y: data.meal3Calories }
-                    ]}
-                    colorScale={["#2d6efd", "#81b4fe", "#dee2e6"]}/>
-                  </Accordion.Body>
+                    <VictoryPie
+                      height={300}
+                      labelRadius={({ innerRadius }) => innerRadius + 40} 
+                      labels={({ datum }) => `${datum.x}\n${datum.y} g`} 
+                      data={[
+                        { x: "Meal 1", y: data.meal1Calories },
+                        { x: "Meal 2", y: data.meal2Calories },
+                        { x: "Meal 3", y: data.meal3Calories }
+                      ]}
+                      colorScale={["#2d6efd", "#81b4fe", "#dee2e6"]}/>
+                    </Accordion.Body>
                 </Accordion.Item>
               </Accordion>
             </Card.Body>
@@ -94,54 +84,54 @@ export default function DataDetails(props) {
                   <Accordion.Item eventKey="0">
                     <Accordion.Header>Exercise 1: {data.exercise1Name}</Accordion.Header>
                     <Accordion.Body>
-                      <p>{data.exercise1Sets} Sets</p>
-                      <p>{data.exercise1Reps} Reps</p>
-                      <p>{data.exercise1Weight} lbs</p>
-                      <p>{data.exercise1Notes}</p>
+                      {data.exercise1Sets} Sets <br />
+                      {data.exercise1Reps} Reps <br />
+                      {data.exercise1Weight} lbs <br />
+                      {data.exercise1Notes} <br />
                     </Accordion.Body>
                   </Accordion.Item>
                   <Accordion.Item eventKey="1">
                     <Accordion.Header>Exercise 2: {data.exercise2Name}</Accordion.Header>
                     <Accordion.Body>
-                      <p>{data.exercise2Sets} Sets</p>
-                      <p>{data.exercise2Reps} Reps</p>
-                      <p>{data.exercise2Weight} lbs</p>
-                      <p>{data.exercise2Notes}</p>
+                      {data.exercise2Sets} Sets <br />
+                      {data.exercise2Reps} Reps <br />
+                      {data.exercise2Weight} lbs <br />
+                      {data.exercise2Notes} <br />
                     </Accordion.Body>
                   </Accordion.Item>
                   <Accordion.Item eventKey="2">
                     <Accordion.Header>Exercise 3: {data.exercise3Name}</Accordion.Header>
                     <Accordion.Body>
-                      <p>{data.exercise3Sets} Sets</p>
-                      <p>{data.exercise3Reps} Reps</p>
-                      <p>{data.exercise3Weight} lbs</p>
-                      <p>{data.exercise3Notes}</p>
+                      {data.exercise3Sets} Sets <br />
+                      {data.exercise3Reps} Reps <br />
+                      {data.exercise3Weight} lbs <br />
+                      {data.exercise3Notes} <br />
                     </Accordion.Body>
                   </Accordion.Item>
                   <Accordion.Item eventKey="3">
                     <Accordion.Header>Exercise 4: {data.exercise4Name}</Accordion.Header>
                     <Accordion.Body>
-                      <p>{data.exercise4Sets} Sets</p>
-                      <p>{data.exercise4Reps} Reps</p>
-                      <p>{data.exercise4Weight} lbs</p>
-                      <p>{data.exercise4Notes}</p>
+                      {data.exercise4Sets} Sets <br />
+                      {data.exercise4Reps} Reps <br />
+                      {data.exercise4Weight} lbs <br />
+                      {data.exercise4Notes} <br />
                     </Accordion.Body>
                   </Accordion.Item>
                   <Accordion.Item eventKey="4">
                     <Accordion.Header>Exercise 5: {data.exercise5Name}</Accordion.Header>
                     <Accordion.Body>
-                      <p>{data.exercise5Sets} Sets</p>
-                      <p>{data.exercise5Reps} Reps</p>
-                      <p>{data.exercise5Weight} lbs</p>
-                      <p>{data.exercise5Notes}</p>
+                      {data.exercise5Sets} Sets <br />
+                      {data.exercise5Reps} Reps <br />
+                      {data.exercise5Weight} lbs <br />
+                      {data.exercise5Notes} <br />
                     </Accordion.Body>
                   </Accordion.Item>
                   <Accordion.Item eventKey="5">
                     <Accordion.Header>Cardio: {data.cardioName}</Accordion.Header>
                     <Accordion.Body>
-                      <p>Duration: {data.cardioDuration} Minutes</p>
-                      <p>Distance: {data.cardioDistance} mi</p>
-                      <p>Notes: {data.cardioNotes}</p>
+                      Duration: {data.cardioDuration} Minutes <br />
+                      Distance: {data.cardioDistance} mi <br />
+                      Notes: {data.cardioNotes} <br />
                     </Accordion.Body>
                   </Accordion.Item>
                 </Accordion>
@@ -156,10 +146,10 @@ export default function DataDetails(props) {
               <Card.Title>Recovery Data</Card.Title>
               <hr />
               <Card.Text>
-                <p>Bedtime: {data.sleepTime}</p>
-                <p>Waketime: {data.wakeTime}</p>
-                <p>Sleep Quality Description: {data.sleepQuality}</p>
-                <p>Sleep rating: {data.sleepScore}/10</p>
+                Bedtime: {data.sleepTime} <br />
+                Waketime: {data.wakeTime} <br />
+                Sleep Quality Description: {data.sleepQuality} <br />
+                Sleep rating: {data.sleepScore}/10 <br />
               </Card.Text>
             </Card.Body>
             <Card.Footer>
@@ -172,8 +162,8 @@ export default function DataDetails(props) {
             <Card.Title>Health Data</Card.Title>
             <hr />
             <Card.Text>
-              <p>Daily Weight: {data.dailyWeight}</p>
-              <p>Daily Status: {data.dailyStatus}</p>
+              Daily Weight: {data.dailyWeight} <br />
+              Daily Status: {data.dailyStatus}
             </Card.Text>
           </Card.Body>
           <Card.Footer>
