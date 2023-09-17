f3e6e77bc002ecd3c194fac1fdf7ffe519737c2a
MarcusKyung <Kyungmj@gmail.com>
2023-09-16 21:16:43 -0700

    update: fix event key numbering

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,11 +21,13 @@ function Header(props) {
             <NavDropdown title="Menu" id="basic-nav-dropdown">
               <NavDropdown.Item eventKey="1" as={Link} to="/about">About</NavDropdown.Item>
               <NavDropdown.Item eventKey="2" as={Link} to="/">Home</NavDropdown.Item>
-              {props.currentUser ? (<NavDropdown.Item eventKey="2" as={Link} to="/sign-in">Sign Out</NavDropdown.Item>) : (<NavDropdown.Item eventKey="2" as={Link} to="/sign-in">Sign In</NavDropdown.Item>)}
+              {props.currentUser ? (<NavDropdown.Item eventKey="3" as={Link} to="/sign-in">Sign Out</NavDropdown.Item>) : (<NavDropdown.Item eventKey="3" as={Link} to="/sign-in">Sign In</NavDropdown.Item>)}
               <NavDropdown.Divider />
-              <NavDropdown.Item eventKey="3" as={Link} to="/tdee">TDEE Calculator</NavDropdown.Item>
-              <NavDropdown.Item eventKey="4" as={Link} to="/1rm">1RM Calculator</NavDropdown.Item>
-              <NavDropdown.Item eventKey="5" as={Link} to="/workout-builder">Workout Builder</NavDropdown.Item>
+              <NavDropdown.Item eventKey="4" as={Link} to="/tdee">TDEE Calculator</NavDropdown.Item>
+              <NavDropdown.Item eventKey="5" as={Link} to="/1rm">1RM Calculator</NavDropdown.Item>
+              <NavDropdown.Item eventKey="6" as={Link} to="/workout-builder">Workout Builder</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item eventKey="7" as={Link} to="/profile">Profile</NavDropdown.Item>
               {props.currentUser && (
                 <div>
                   <NavDropdown.Divider />
